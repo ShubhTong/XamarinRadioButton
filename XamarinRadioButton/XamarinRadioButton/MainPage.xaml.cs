95965a667549a2f6db92da6e504b9cac8ca523bc
shubhtong <shubh.jain1993@gmail.com>
2020-07-01 17:43:50 +0530

    first commit

---
+++ XamarinRadioButton/XamarinRadioButton/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace XamarinRadioButton
+{
+
+    public partial class MainPage : ContentPage
+    {
+        private List<RadioButtonControl> radioBtnCtrlList;
+        private const string radioBtnGroupName = "transactionRadioBtn";
+        public MainPage()
+        {
+            InitializeComponent();
+            radioBtnCtrlList = new List<RadioButtonControl>();
+
+            var dataList = getDataMOdels();
+
+            listView.ItemsSource = dataList;
+        }
+
+        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {           
+            radioBtnCtrlList[e.SelectedItemIndex].RadioBtnTapHandler(null, null);
+        }
+
+        private void OnCheckedChanged(object sender, EventArgs e)
+        {
+            var selectedRad = sender as RadioButtonControl;
+
+            foreach (var rad in radioBtnCtrlList)
+            {
+                if (!selectedRad.Uid.Equals(rad.Uid))
+                {
+                    rad.Checked = false;
+                }
+                else
+                {
+                    rad.Checked = true;
+                }
+            }
+        }
+
+        private void transactionList_ChildAdded(object sender, ElementEventArgs e)
+        {
+            RadioButtonControl radioBtnCtrl = null;
+
+            if (e.Element.ClassId == radioBtnGroupName)
+            {
+                radioBtnCtrl = e.Element as RadioButtonControl;                
+                radioBtnCtrl.CheckedChanged = OnCheckedChanged;
+                radioBtnCtrlList.Add(radioBtnCtrl);
+            }
+
+        }
+
+        private List<DataModel> getDataMOdels()
+        {
+            return new List<DataModel>()
+            {
+               new DataModel() {UniqNo = 1, Name = "shubh", Amount=111, Date="01/10/2020", Description= "Amazon"},
+               new DataModel() {UniqNo = 2, Name = "s", Amount=10, Date="01/10/2020", Description= "Google"},
+               new DataModel() {UniqNo = 3, Name = "j", Amount=120, Date="01/10/2020", Description= "FB"},
+               new DataModel() {UniqNo = 4, Name = "sj", Amount=1200, Date="01/10/2020", Description= "Netflix"},
+               new DataModel() {UniqNo = 5, Name = "SJ", Amount=1, Date="01/10/2020", Description= "Adobe"}
+
+            };
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+             radioBtnCtrlList.Select(r => r.BorderColor = Color.Red).ToList();
+            //foreach(var rad in radioBtnCtrlList)
+            //{
+            //    rad.BorderColor = Color.Red;
+            //}
+        }        
+    }
+
+    public class DataModel
+    {
+        public int UniqNo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+        public int Amount { get; set; }
+    }
+}
---
+++ XamarinRadioButton/XamarinRadioButton/RadioButtonControl.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinRadioButton
+{
+    public class RadioButtonControl : StackLayout
+    {
+        private bool _isChecked = false;
+        private BoxView _innerCircle = new BoxView();
+        private Button _radioBtn = null;
+
+        public static readonly BindableProperty CheckedProperty =
+        BindableProperty.Create(nameof(Checked), typeof(bool), typeof(RadioButtonControl), false, defaultBindingMode: BindingMode.TwoWay);
+
+        public static readonly BindableProperty UidProperty =
+        BindableProperty.Create(nameof(Uid), typeof(int), typeof(RadioButtonControl), 0, defaultBindingMode: BindingMode.TwoWay);
+
+        public EventHandler CheckedChanged;
+        public bool Checked
+        {
+            get
+            {
+                return (bool)GetValue(CheckedProperty);
+            }
+
+            set
+            {
+                SetValue(CheckedProperty, value);
+                _isChecked = value;
+                UpdateRadioBtnState();
+            }
+        }
+
+        public int Uid
+        {
+            get
+            {
+                return (int)GetValue(UidProperty);
+            }
+
+            set
+            {
+                SetValue(UidProperty, value);
+            }
+        }
+
+        public Color BorderColor
+        {
+            set
+            {
+                _radioBtn.BorderColor = value;
+            }
+        }
+
+        public RadioButtonControl()
+        {
+            Orientation = StackOrientation.Horizontal;
+            HorizontalOptions = LayoutOptions.FillAndExpand;
+            Padding = new Thickness(10, 0);
+            Spacing = 0;
+            CreateRadioBtn();
+            RegisterEvents();
+        }
+
+        private void CreateRadioBtn()
+        {
+            _radioBtn = new Button()
+            {
+                Padding = 0,
+                HeightRequest = 40,
+                WidthRequest = 40,
+                IsEnabled = false,
+                Style = GetDefaultStyle()
+            };
+
+            this.Children.Add(_radioBtn);
+
+            _innerCircle = new BoxView()
+            {
+                CornerRadius = 15,
+                BackgroundColor = Color.Transparent,
+                HeightRequest = 25,
+                WidthRequest = 25,
+                Parent = _radioBtn,
+                Margin = new Thickness(-32.5, 0.6, 0, 0),
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+            };
+
+            this.Children.Add(_innerCircle);
+        }
+
+        private void RegisterEvents()
+        {
+            TapGestureRecognizer tap = new TapGestureRecognizer();
+            tap.Tapped += RadioBtnTapHandler;
+            _innerCircle.GestureRecognizers.Add(tap);
+        }
+
+        public void RadioBtnTapHandler(object sender, EventArgs e)
+        {
+            var innerCircle = sender as BoxView;
+
+            if (!_isChecked)
+            {
+                _isChecked = true;
+                _innerCircle.BackgroundColor = Color.ForestGreen;
+            }
+            else
+            {
+                _isChecked = false;
+                _innerCircle.BackgroundColor = Color.Transparent;
+            }
+            CheckedChanged?.Invoke(this, e);
+        }
+
+        private void UpdateRadioBtnState()
+        {
+            if (_isChecked)
+            {
+                _innerCircle.BackgroundColor = Color.ForestGreen;
+            }
+            else
+            {
+                _innerCircle.BackgroundColor = Color.White;
+            }
+        }
+        private Style GetDefaultStyle()
+        {
+            return new Style(typeof(Button))
+            {
+                Setters = {
+                    new Setter { Property = BackgroundColorProperty,   Value = Color.Transparent },
+                    new Setter { Property = Button.BorderColorProperty,   Value = Color.LightGray },
+                    new Setter { Property = Button.TextColorProperty,   Value = Color.ForestGreen },
+                    new Setter { Property = Button.BorderWidthProperty,   Value = 3 },
+                    new Setter { Property = Button.CornerRadiusProperty,   Value = 20 },
+                    new Setter { Property = HeightRequestProperty,   Value = 40 },
+                    new Setter { Property = WidthRequestProperty,   Value = 40 }
+            }
+            };
+
+        }
+    }
+}
